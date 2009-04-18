938a441599d6aa08c7ca2ca6573ee731c0fa3534
hywan <devnull@localhost>
2009-04-18 17:07:56 +0000

    If we want to flush, it must be on a Hoa_Stream_Io stream at least.

--- Framework/Stream/Io/Bufferable.php
+++ Framework/Stream/Io/Bufferable.php
@@ -57,8 +57,8 @@ interface Hoa_Stream_Io_Bufferable {
      * Flush the output to a stream.
      *
      * @access  public
-     * @param   Hoa_Stream  $stream    Stream to flush.
+     * @param   Hoa_Stream_Io  $stream    Stream to flush.
      * @return  bool
      */
-    public function flush ( Hoa_Stream $stream );
+    public function flush ( Hoa_Stream_Io $stream );
 }
