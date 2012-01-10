6412d44f1bc1b1dc9fac944c8548ed72c72d0003
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2012-01-10 15:07:17 +0100

    s/2007-2011/2007-2012/

--- Library/Stream/Filter/Basic.php
+++ Library/Stream/Filter/Basic.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2011, Ivan Enderlin. All rights reserved.
+ * Copyright © 2007-2012, Ivan Enderlin. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -59,7 +59,7 @@ namespace Hoa\Stream\Filter {
  * Actually, it extends the php_user_filter class.
  *
  * @author     Ivan Enderlin <ivan.enderlin@hoa-project.net>
- * @copyright  Copyright © 2007-2011 Ivan Enderlin.
+ * @copyright  Copyright © 2007-2012 Ivan Enderlin.
  * @license    New BSD License
  */
 
